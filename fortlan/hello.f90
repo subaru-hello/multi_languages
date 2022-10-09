c956f2dd81565773a3ac706c04738fb46c3a5848
subaru-hello <octosubaru0926@gmail.com>
2022-10-09 16:40:04 +0900

    fortlan

---
+++ fortlan/hello.f90
@@ -0,0 +1,8 @@
+PROGRAM HELLO_WORLD
+    write(*,*) 'Hello, world !'
+    stop
+END PROGRAM HELLO_WORLD
+
+! gfortran hello.f90 で実行され、コンパイル後のファイル（a.out）が出力される
+! ./fortlan/a.out で下記が出力される
+! Hello World
---
+++ fortlan/variable.f90
@@ -0,0 +1,21 @@
+program variable
+    implicit none ! 暗黙の型宣言禁止
+  
+    ! 変数を使う前には必ず以下のように宣言を行う
+    integer :: n ! 整数型の変数の宣言
+    real :: x    ! 実数型の変数の宣言
+  
+    ! 代入
+    n = 10
+    x = 3.14
+  
+    ! 表示
+    write(*,*) 'integer => ', n
+    write(*,*) 'real => ', x
+  
+    stop
+  end program variable
+
+!   ./fortlan/variable で出力される値
+!  integer =>           10
+!  real =>    3.14000010    
